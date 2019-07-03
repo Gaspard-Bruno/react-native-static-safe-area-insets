a6f7bf002fb611d227e6782562765feaf213a3ed
danielabdelsamed <daniel@abdelsamed.com>
2019-07-02 22:19:50 -0400

    Added typescript types

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module "react-native-static-safe-are-insets" {
+  export default interface StaticSafeAreaInsets {
+    safeAreaInsetsTop: number;
+    safeAreaInsetsBottom: number;
+    safeAreaInsetsLeft: number;
+    safeAreaInsetsRight: number;
+  }
+}
