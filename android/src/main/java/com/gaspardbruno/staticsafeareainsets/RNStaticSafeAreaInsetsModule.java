840ef0ccdeb33dd34f5b6caa713a925ae5e5894d
Rapsssito <rodrigo712000@gmail.com>
2019-12-15 00:39:26 +0100

    Fix for views drawing under the navbar & statusbar

--- android/src/main/java/com/gaspardbruno/staticsafeareainsets/RNStaticSafeAreaInsetsModule.java
+++ android/src/main/java/com/gaspardbruno/staticsafeareainsets/RNStaticSafeAreaInsetsModule.java
@@ -38,7 +38,7 @@ public class RNStaticSafeAreaInsetsModule extends ReactContextBaseJavaModule {
     // Window insets are parts of the window that are covered by system views (status bar,
     // navigation bar, notches). There are no apis the get these values for android < M so we
     // do a best effort polyfill.
-    EdgeInsets windowInsets;
+    EdgeInsets windowInsets = new EdgeInsets(0, 0, 0, 0);
     final Activity activity = getCurrentActivity();
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
       final View view = activity.getWindow().getDecorView();
@@ -48,38 +48,40 @@ public class RNStaticSafeAreaInsetsModule extends ReactContextBaseJavaModule {
           insets.getSystemWindowInsetRight(),
           insets.getSystemWindowInsetBottom(),
           insets.getSystemWindowInsetLeft());
-    } else {
-      final Resources resources = mReactContext.getResources();
-      int rotation = mWindowManager.getDefaultDisplay().getRotation();
-      int statusBarHeight = 0;
-      int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
-      if (resourceId > 0) {
-        statusBarHeight = Math.round(PixelUtil.toDIPFromPixel(resources.getDimensionPixelSize(resourceId)));
-      }
-      int navbarHeight = 0;
-      resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
-      if (resourceId > 0) {
-        navbarHeight = Math.round(PixelUtil.toDIPFromPixel(resources.getDimensionPixelSize(resourceId)));
-      }
-
-      windowInsets = new EdgeInsets(
-          statusBarHeight,
-          rotation == Surface.ROTATION_90 ? navbarHeight : 0,
-          rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180 ? navbarHeight : 0,
-          rotation == Surface.ROTATION_270 ? navbarHeight : 0);
     }
+    final Resources resources = mReactContext.getResources();
+    final int rotation = mWindowManager.getDefaultDisplay().getRotation();
+    float statusBarHeight = 0;
+    int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
+    if (resourceId > 0) {
+      statusBarHeight = PixelUtil.toDIPFromPixel(resources.getDimensionPixelSize(resourceId));
+    }
+    float navbarHeight = 0;
+    resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
+    if (resourceId > 0) {
+      navbarHeight = PixelUtil.toDIPFromPixel(resources.getDimensionPixelSize(resourceId));
+    }
+    // Fix for views drawing under the navbar & statusbar
+    windowInsets = new EdgeInsets(
+        windowInsets.top != 0 ? windowInsets.top : statusBarHeight,
+        windowInsets.right != 0 ? windowInsets.right : rotation == Surface.ROTATION_90 ? navbarHeight : 0,
+        windowInsets.bottom != 0 ? windowInsets.bottom : rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180 ? navbarHeight : 0,
+        windowInsets.left != 0 ? windowInsets.left : rotation == Surface.ROTATION_270 ? navbarHeight : 0);
+
 
     // Calculate the part of the root view that overlaps with window insets.
-    View rootView = activity.getWindow().getDecorView().findViewById(android.R.id.content).getRootView();
-    View contentView = rootView.findViewById(android.R.id.content);
-    float windowWidth = rootView.getWidth();
-    float windowHeight = rootView.getHeight();
-    int[] windowLocation = new int[2];
-    contentView.getLocationInWindow(windowLocation);
-    windowInsets.top = Math.max(windowInsets.top - windowLocation[1], 0);
-    windowInsets.left = Math.max(windowInsets.left - windowLocation[0], 0);
-    windowInsets.bottom = Math.max(windowLocation[1] + contentView.getHeight() + windowInsets.bottom - windowHeight, 0);
-    windowInsets.right = Math.max(windowLocation[0] + contentView.getWidth() + windowInsets.right - windowWidth, 0);
+    if (activity != null) {
+      final View rootView = activity.getWindow().getDecorView().getRootView();
+      final View contentView = rootView.findViewById(android.R.id.content);
+      final float windowWidth = rootView.getWidth();
+      final float windowHeight = rootView.getHeight();
+      final int[] windowLocation = new int[2];
+      contentView.getLocationInWindow(windowLocation);
+      windowInsets.top = Math.max(windowInsets.top - windowLocation[1], 0);
+      windowInsets.left = Math.max(windowInsets.left - windowLocation[0], 0);
+      windowInsets.bottom = Math.max(windowLocation[1] + contentView.getHeight() + windowInsets.bottom - windowHeight, 0);
+      windowInsets.right = Math.max(windowLocation[0] + contentView.getWidth() + windowInsets.right - windowWidth, 0);
+    }
 
     constants.put("safeAreaInsetsTop", windowInsets.top);
     constants.put("safeAreaInsetsBottom", windowInsets.bottom);
