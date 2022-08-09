4967b1dc85337a18069c704c6c267cef99d74924
João Horta <joao@gaspardbruno.com>
2022-08-09 17:10:47 +0100

    feat: android insets logic improved for api 30+

--- android/src/main/java/com/gaspardbruno/staticsafeareainsets/RNStaticSafeAreaInsetsModule.java
+++ android/src/main/java/com/gaspardbruno/staticsafeareainsets/RNStaticSafeAreaInsetsModule.java
@@ -13,8 +13,10 @@ import java.util.HashMap;
 
 import android.view.WindowInsets;
 import android.view.View;
+import android.view.WindowInsetsController;
 import android.os.Build;
 import android.app.Activity;
+import android.graphics.Insets;
 
 public class RNStaticSafeAreaInsetsModule extends ReactContextBaseJavaModule {
 
@@ -43,10 +45,31 @@ public class RNStaticSafeAreaInsetsModule extends ReactContextBaseJavaModule {
       final View view = activity.getWindow().getDecorView();
       final WindowInsets insets = view.getRootWindowInsets();
 
-      constants.put("safeAreaInsetsTop", PixelUtil.toDIPFromPixel(insets.getSystemWindowInsetTop()));
-      constants.put("safeAreaInsetsBottom", PixelUtil.toDIPFromPixel(insets.getSystemWindowInsetBottom()));
-      constants.put("safeAreaInsetsLeft", PixelUtil.toDIPFromPixel(insets.getSystemWindowInsetLeft()));
-      constants.put("safeAreaInsetsRight", PixelUtil.toDIPFromPixel(insets.getSystemWindowInsetRight()));
+      final Boolean isFullscreen = Build.VERSION.SDK_INT >= Build.VERSION_CODES.R ?
+        view.getWindowInsetsController().getSystemBarsBehavior() == WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        :
+        view.getWindowSystemUiVisibility() == View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+
+      if (isFullscreen) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+          final Insets insets30 = insets.getInsets(WindowInsets.Type.systemGestures());
+
+          constants.put("safeAreaInsetsTop", insets30.top);
+          constants.put("safeAreaInsetsBottom", insets30.bottom);
+          constants.put("safeAreaInsetsLeft", insets30.left);
+          constants.put("safeAreaInsetsRight", insets30.right);
+        } else {
+          constants.put("safeAreaInsetsTop", PixelUtil.toDIPFromPixel(insets.getSystemWindowInsetTop()));
+          constants.put("safeAreaInsetsBottom", PixelUtil.toDIPFromPixel(insets.getSystemWindowInsetBottom()));
+          constants.put("safeAreaInsetsLeft", PixelUtil.toDIPFromPixel(insets.getSystemWindowInsetLeft()));
+          constants.put("safeAreaInsetsRight", PixelUtil.toDIPFromPixel(insets.getSystemWindowInsetRight()));
+        }
+      } else {
+        constants.put("safeAreaInsetsTop", 0f);
+        constants.put("safeAreaInsetsBottom", 0f);
+        constants.put("safeAreaInsetsLeft", 0f);
+        constants.put("safeAreaInsetsRight", 0f);
+      }
     } else {
       constants.put("safeAreaInsetsTop", 0f);
       constants.put("safeAreaInsetsBottom", 0f);
